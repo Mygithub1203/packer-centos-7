9c2e266518263596a38ceb3cbd3cfc6ea88be5c4
François Garillot <francois@garillot.net>
2017-03-23 17:29:06 -0400

    Debug azure credentials script

--- azure_credentials.sh
+++ azure_credentials.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 # requires jq & azure cli
 echo "This should only be executed after you have logged in with azure login -u username"
@@ -11,16 +11,21 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     APPURL="http://provisioning.deeplearn.online"
     export ARM_CLIENT_SECRET=$(openssl rand -base64 24)
     ARM_SUBSCRIPTION_ID=$(azure account show --json | jq ".[] | .id")
-    export ARM_SUBSCRIPTION_ID=${${ARM_SUBSCRIPTION_ID%\"}#\"}
+    ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID%\"}
+    export ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID#\"}
 
     azure ad app create -n "centos7-provisioning" -i $APPURL --home-page $APPURL -p $ARM_CLIENT_SECRET
+    sleep 30 # app ID registration takes time
     ARM_CLIENT_ID=$(azure ad app list --json |  jq '.[] | select(.displayName | contains("centos7-provisioning")) | .appId')
-    export ARM_CLIENT_ID=${${ARM_CLIENT_ID%\"}#\"}
+    ARM_CLIENT_ID=${ARM_CLIENT_ID%\"}
+    export ARM_CLIENT_ID=${ARM_CLIENT_ID#\"}
     azure ad sp create --applicationId $ARM_CLIENT_ID
+    sleep 30
     azure role assignment create --spn $APPURL -o "Owner" -c /subscriptions/$ARM_SUBSCRIPTION_ID
 
     export ARM_RESOURCE_GROUP="centos7-provisioning"
     export ARM_STORAGE_ACCOUNT="centos7storageprovision"
     ARM_TENANT_ID=$(azure account show --json | jq ".[] | .user.name")
-    export ARM_TENANT_ID=${${ARM_TENANT_ID%\"}#\"}
+    ARM_TENANT_ID=${ARM_TENANT_ID%\"}
+    export ARM_TENANT_ID=${ARM_TENANT_ID#\"}
 fi
