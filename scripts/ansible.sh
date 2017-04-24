dda895ff90c7af87063e27fc8dcdf98cc0fe4a02
François Garillot <francois@garillot.net>
2017-04-24 09:51:52 -0400

    force updates before installing pip

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eux
 
 # Install EPEL repository.
-yum -y install epel-release python-pip yum-utils gcc openssl-devel
-
+yum -y install epel-release yum-utils gcc openssl-devel
+yum -y update
+yum -y install python-pip python-wheel
+yum -y  upgrade python-setuptools
 yum-builddep -y ansible
 
 # Install Ansible.
