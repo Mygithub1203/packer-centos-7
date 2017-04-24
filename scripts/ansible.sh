0d25eb627ce28fb245c93b984d07d3a0b065bb14
François Garillot <francois@garillot.net>
2017-04-24 05:56:32 -0400

    require a newer ansible (java_deps requires advanced yum_repo options)

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -eux
 
 # Install EPEL repository.
-yum -y install epel-release
+yum -y install epel-release python-pip yum-utils gcc openssl-devel
+
+yum-builddep -y ansible
 
 # Install Ansible.
-yum -y install ansible
+# yum -y install ansible
+pip install --upgrade pip
+pip install --upgrade ansible
