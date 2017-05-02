49399ac488179ed1468c9241328a73dbdea56496
François Garillot <francois@garillot.net>
2017-05-02 11:06:04 -0400

    Set entrypoint

--- azure_credentials.sh
+++ azure_credentials.sh
@@ -29,7 +29,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
     export ARM_RESOURCE_GROUP="centos7-provisioning$(openssl rand -hex 4)"
     azure group create -n "$ARM_RESOURCE_GROUP" -l eastus
-    export ARM_STORAGE_ACCOUNT="centos7storage$(openssl rand -hex 4)"
+    # export ARM_STORAGE_ACCOUNT="centos7storage$(openssl rand -hex 4)"
+    export ARM_STORAGE_ACCOUNT="centos7storageprovision"
     azure storage account create -g $ARM_RESOURCE_GROUP -l eastus --sku-name LRS --kind storage $ARM_STORAGE_ACCOUNT
 
     ARM_TENANT_ID=$(azure account show --json | jq ".[] | .user.name")
